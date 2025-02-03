bbd18c7e12de4b272b516d35835f29f905e5b4b5
fernandobouchet <fernandobouchet@gmail.com>
2025-02-03 18:10:51 -0300

    feat: add types

---
+++ types.d.ts
@@ -0,0 +1,30 @@
+interface Task {
+  id: string | undefined;
+  title: string;
+  description: string;
+  dueDate: Date | undefined;
+  priority: TaskPriority;
+  status: TaskStatus | undefined;
+}
+
+interface TaskList {
+  id: string | undefined;
+  title: string;
+  description: string | undefined;
+  count: number | undefined;
+  progress: number | undefined;
+  tasks: Task[] | undefined;
+}
+
+type NewTaskListForm = Pick<TaskList, "title" | "description">;
+
+enum TaskPriority {
+  HIGH = "HIGH",
+  MEDIUM = "MEDIUM",
+  LOW = "LOW",
+}
+
+enum TaskStatus {
+  OPEN = "OPEN",
+  CLOSED = "CLOSED",
+}
